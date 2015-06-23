e7b1778dac9a3bd109621ae78964add0c7b94210
Wade Berrier <wberrier@gmail.com>
2015-06-22 20:35:35 -0600

    emacs: Fix encryption in terminal for some versions of emacs (el7)

--- site-start.d/50encryption.el
+++ site-start.d/50encryption.el
@@ -1,6 +1,11 @@
 (require 'epa-file)
 (epa-file-enable)
 
-; things don't work right when calling out to pinentry...
-; just bypass it
-;(setenv "GPG_AGENT_INFO" nil)
+; ok, on some versions of emacs we need to use the old gpg in order to
+; for terminal pin entries not to hang
+; download compile and install gpg1 from https://www.gnupg.org/download/
+(setq gpg1_path (format "~/install/%s/bin/gpg" dist))
+(when(file-executable-p gpg1_path)
+  (setq epg-gpg-program gpg1_path)
+  )
+
