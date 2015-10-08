40a44a6597688bd568ff208536a87824809e12a3
Wade Berrier <wberrier@gmail.com>
2015-10-08 01:06:04 -0600

    emacs: set up magit

---
+++ site-start.d/90magit.el
@@ -0,0 +1,15 @@
+
+;; git-modes: https://github.com/magit/git-modes
+(add-to-list 'load-path "~/wa/git/git-modes")
+(require 'gitattributes-mode)
+(require 'gitconfig-mode)
+(require 'gitignore-mode)
+; (require 'git-rebase-mode) ;; doesn't work well with evil
+
+; what provides this?
+; Seems to be missing on fedora 22
+; don't fail if doesn't exist
+(when (require 'git-commit-mode nil 'noerror))
+
+(add-to-list 'load-path "~/wa/git/magit")
+(require 'magit)
