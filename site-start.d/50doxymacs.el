a79a200fa0e382abb3746c566bab8788f73240f1
Wade Berrier <wberrier@gmail.com>
2016-09-27 10:25:18 -0600

    remove a note that has been migrated to a markdown note

--- site-start.d/50doxymacs.el
+++ site-start.d/50doxymacs.el
@@ -8,3 +8,4 @@
 
 (autoload 'markdown-mode "markdown-mode" "Major mode for editing Markdown files" t)
 (add-to-list 'auto-mode-alist '("\\.dox\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
