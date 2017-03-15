b83c0fd94c17db6f0db1b15c608fa43563dd20d1
Wade Berrier <wberrier@gmail.com>
2017-03-14 21:42:36 -0600

    remove some references to the old git dirs

--- site-start.d/50theme.el
+++ site-start.d/50theme.el
@@ -4,15 +4,12 @@
 
 ;; Nice monokai color theme
 ; seems to only work in 256 color mode
-;(add-to-list 'custom-theme-load-path "~/wa/git/monokai-emacs")
 ;(load-theme 'monokai t)
 
-;(add-to-list 'custom-theme-load-path "~/wa/git/zenburn-emacs")
 ; seems to only work in 256 color mode, but has a nice 8 color fallback option
 ;(load-theme 'zenburn t)
 
 ; Very nice theme
-;(add-to-list 'custom-theme-load-path "~/wa/git/emacs-color-theme-solarized")
 ;(setq solarized-termcolors 256) ; use 256 terminal color emulation
 ; forces dark theme
 ;(set-terminal-parameter nil 'background-mode 'dark)
@@ -33,12 +30,9 @@
 (load-theme 'ample t)
 
 ; sanityinc themes, can't seem to get them to work
-;(add-to-list 'custom-theme-load-path "~/wa/git/color-theme-sanityinc-tomorrow")
 ;(require 'color-theme-sanityinc-tomorrow)
 ;(load-theme 'ample t)
 
-;(add-to-list 'custom-theme-load-path "~/wa/git/cyberpunk-theme.el")
 ;(load-theme 'cyberpunk t)
 
-;(add-to-list 'custom-theme-load-path "~/wa/git/moe-theme.el")
 ;(load-theme 'moe t)
