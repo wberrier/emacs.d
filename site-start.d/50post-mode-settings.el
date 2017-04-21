e154f14aebba520c268fe99fffcfe2fbfb71f49c
Wade Berrier <wberrier@gmail.com>
2017-04-21 08:14:13 -0600

    mutt: stricter file glob for mutt messages.
    Otherwise files starting with mutt were always in post-mode

--- site-start.d/50post-mode-settings.el
+++ site-start.d/50post-mode-settings.el
@@ -2,7 +2,7 @@
 ;; Post Mode
 ;; mutt composition mode
 
-(add-to-list 'auto-mode-alist '("\\.*mutt-*\\|.article\\|\\.followup" . post-mode))
+(add-to-list 'auto-mode-alist '("tmp/mutt*\\|\\.article|\\.followup" . post-mode))
 (add-to-list 'auto-mode-alist '("/tmp/evo*" . post-mode))
 
 (require 'post)
