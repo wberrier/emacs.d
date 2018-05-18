5fd458e6e6b80f17e49e08ab3fe65cfcb038fa11
Wade Berrier <wberrier@gmail.com>
2018-05-18 15:17:43 -0600

    git commit mode: force evil normal state
    otherwise defaults to emacs state

--- site-start.d/90vcs.el
+++ site-start.d/90vcs.el
@@ -3,7 +3,11 @@
 ;; (use backend agnostic vc for normal stuff)
 
 ;; git
-(use-package git-commit)
+(use-package git-commit
+  :config
+  ;; git-commit mode wants to go into emacs state.  Force evil state.
+  (add-hook 'with-editor-mode-hook 'evil-normal-state)
+  )
 (use-package gitconfig-mode)
 (use-package gitignore-mode)
 (use-package gitattributes-mode)
