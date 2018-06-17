872a1ee28522d2190ceb142c02be9478a20445ac
Wade Berrier <wberrier@gmail.com>
2018-06-17 15:41:48 -0600

    emacsproject bin files: mostly whitespace changes, pylint fixes
    use main() functions, and other various fixes

--- bin/generate-compile-commands.py
+++ bin/generate-compile-commands.py
@@ -1,46 +1,57 @@
 #!/usr/bin/env python3
 
+"""Modify compile_commands.json for cquery depending on emacsproject config"""
+
+# Don't complain about dashes in script name, I like them better than underscores
+# pylint: disable=locally-disabled, invalid-name
+
 import json
 import os
 import sys
 import emacsproject.config
 
-project_dir = sys.argv[1]
-cquery_config_file = project_dir + os.sep + emacsproject.config.FILENAME
+def main():
+    """main"""
 
-# Try to read config file
-config = emacsproject.config.load_json_file(cquery_config_file)
+    project_dir = sys.argv[1]
+    cquery_config_file = project_dir + os.sep + emacsproject.config.FILENAME
 
-# get values (with some fallback defaults)
-working_dir       = config['cquery'].get('working_dir', 'build')
-override_compiler = config['cquery'].get('override_compiler', False)
-ignore_args       = config['cquery'].get('ignore_args', '').split()
+    # Try to read config file
+    config = emacsproject.config.load_json_file(cquery_config_file)
 
-with open(os.path.join(project_dir, working_dir, 'compile_commands.json')) as f:
+    # get values (with some fallback defaults)
+    working_dir = config['cquery'].get('working_dir', 'build')
+    override_compiler = config['cquery'].get('override_compiler', False)
+    ignore_args = config['cquery'].get('ignore_args', '').split()
 
-    db = json.load(f)
+    with open(os.path.join(project_dir, working_dir, 'compile_commands.json')) as descriptor:
 
-    for entry in db:
-        command = entry['command']
+        commands_db = json.load(descriptor)
 
-        args = command.split()
+        for entry in commands_db:
+            command = entry['command']
 
-        new_args = []
+            args = command.split()
 
-        # Sanitize compile_commands.json
-        for arg in args:
+            new_args = []
 
-            if override_compiler:
-                if arg.endswith("gcc"):
-                    arg = "clang"
-                elif arg.endswith("g++"):
-                    arg = "clang++"
+            # Sanitize compile_commands.json
+            for arg in args:
 
-            if arg in ignore_args:
-                arg = ""
+                if override_compiler:
+                    if arg.endswith("gcc"):
+                        arg = "clang"
+                    elif arg.endswith("g++"):
+                        arg = "clang++"
 
-            new_args.append(arg)
+                if arg in ignore_args:
+                    arg = ""
 
-        entry['command'] = " ".join(new_args)
+                new_args.append(arg)
 
-    json.dump(db, sys.stdout)
+            entry['command'] = " ".join(new_args)
+
+        json.dump(commands_db, sys.stdout)
+
+if __name__ == "__main__":
+    main()
