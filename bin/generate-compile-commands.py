ef148d8adba08d35ed35b6c2902ae67511e2b7ca
Wade Berrier <wberrier@gmail.com>
2018-06-21 11:20:58 -0600

    generate-compile-commands: all regex substitutions

--- bin/generate-compile-commands.py
+++ bin/generate-compile-commands.py
@@ -9,6 +9,15 @@ import json
 import os
 import sys
 import emacsproject.config
+import re
+
+def perform_substitutions(line, regex_map):
+    """Generic function to exercise a bunch of regexes on some text"""
+
+    for regex, new_text in regex_map.items():
+        line = re.sub(regex, new_text, line)
+
+    return line
 
 def main():
     """main"""
@@ -24,11 +33,26 @@ def main():
     override_compiler = config['cquery'].get('override_compiler', False)
     ignore_args = config['cquery'].get('ignore_args', '').split()
 
+    # Process some filter regexes
+    filter_regexes = config['cquery'].get('filter_regexes', {})
+    directory_filter_regexes = config['cquery'].get('directory_filter_regexes', {})
+    command_filter_regexes = config['cquery'].get('command_filter_regexes', {})
+    file_filter_regexes = config['cquery'].get('file_filter_regexes', {})
+
     with open(os.path.join(project_dir, working_dir, 'compile_commands.json')) as descriptor:
 
         commands_db = json.load(descriptor)
 
         for entry in commands_db:
+
+            # Perform all filter regexs
+            for i in ['directory', 'command', 'file']:
+                entry[i] = perform_substitutions(entry[i], filter_regexes)
+
+            entry['directory'] = perform_substitutions(entry['directory'], directory_filter_regexes)
+            entry['command'] = perform_substitutions(entry['command'], command_filter_regexes)
+            entry['file'] = perform_substitutions(entry['file'], file_filter_regexes)
+
             command = entry['command']
 
             args = command.split()
