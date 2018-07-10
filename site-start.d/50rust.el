9a51e0d42218794460d0f6efcee6d5d6bb3cb985
Wade Berrier <wberrier@gmail.com>
2018-07-09 21:59:05 -0600

    rust: configure lsp
    Seems to work on a basic example...

--- site-start.d/50rust.el
+++ site-start.d/50rust.el
@@ -1,31 +1,11 @@
 
-;; TODO: use lsp-rust
-
-;; flycheck and tags
-
-;; Dependencies
-(use-package racer)
-(use-package flycheck-rust)
-(use-package cargo)
+;; lsp-rust
+(use-package lsp-rust
+  :config
+  (add-hook 'rust-mode-hook 'lsp-rust-enable)
+  (add-hook 'rust-mode-hook 'flycheck-mode)
+  ;;(setq lsp-rust-rls-command '("rls"))
+  )
 
 ;; rust-mode
-(use-package rust-mode
-
-  :init
-  (setq company-tooltip-align-annotations t)
-
-  (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
-
-  (add-hook 'rust-mode-hook #'racer-mode)
-  (add-hook 'racer-mode-hook #'eldoc-mode)
-
-  (add-hook 'rust-mode-hook #'cargo-minor-mode)
-
-  ;; Code completion
-  (add-hook 'racer-mode-hook #'company-mode)
-
-  :general
-  ;; TODO: necessary?
-  (:keymaps 'rust-mode-map "TAB" 'company-indent-or-complete-common)
-
-  )
+(use-package rust-mode)
