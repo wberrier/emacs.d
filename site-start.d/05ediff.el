a0f82ec0f44bc4faf23bb069e7b99a2a385c810e
Wade Berrier <wberrier@gmail.com>
2018-08-21 14:48:43 -0600

    ediff: don't prompt on quit
    can always remove this if it becomes problematic

--- site-start.d/05ediff.el
+++ site-start.d/05ediff.el
@@ -4,3 +4,11 @@
 (setq-default ediff-forward-word-function 'forward-char)
 ;; split vertically by default instead of horizontally
 (setq-default ediff-split-window-function 'split-window-horizontally)
+
+;; don't prompt for quit
+;; got this on stack overflow
+(defun disable-y-or-n-p (orig-fun &rest args)
+  (cl-letf (((symbol-function 'y-or-n-p) (lambda (prompt) t)))
+	(apply orig-fun args)))
+
+(advice-add 'ediff-quit :around #'disable-y-or-n-p)
