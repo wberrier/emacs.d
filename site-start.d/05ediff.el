d52b5532517097ec3df1b0d5de2f276fe5c265d7
Wade Berrier <wberrier@gmail.com>
2018-08-21 11:55:58 -0600

    ediff: add some defaults to make it more manageable

---
+++ site-start.d/05ediff.el
@@ -0,0 +1,6 @@
+
+;; edif Settings
+;; character level diffs
+(setq-default ediff-forward-word-function 'forward-char)
+;; split vertically by default instead of horizontally
+(setq-default ediff-split-window-function 'split-window-horizontally)
