ee96f6fcf7e7f6d9ca904c20d58f301c1e8d13e4
Wade Berrier <wberrier@gmail.com>
2018-09-27 10:42:04 -0600

    java lsp: don't mess with imports
    at least in one case it removed some imports, causing a normally
    working build to fail

--- site-start.d/60java.el
+++ site-start.d/60java.el
@@ -4,4 +4,7 @@
   (add-hook 'java-mode-hook 'lsp-java-enable)
   (add-hook 'java-mode-hook 'flycheck-mode)
   ;;(setq lsp-java--workspace-folders (list (error "XXX Specify your projects here")))
+  :custom
+  (lsp-java-organize-imports nil)
+  (lsp-java-save-action-organize-imports nil)
   )
