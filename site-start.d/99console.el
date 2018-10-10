e14076aa762db387d4357b2ae36b0d9cc9f4f77a
Wade Berrier <wberrier@gmail.com>
2018-10-10 10:52:35 -0600

    console settings: copy/paste when background set
    I think I commented these out previously because they didn't work
    
    Probably just have to toggle global-font-lock-mode

--- site-start.d/99console.el
+++ site-start.d/99console.el
@@ -1,9 +1,11 @@
-; Either of these work to avoid setting the background color,
-; which messes up copying and pasting out of emacs
+;; "unset" the background color, which messes up copying and pasting
+;; out of emacs When running from the console
+;; (unless window-system
+;;   (set-face-attribute 'default nil :background "unspecified-bg")
+;;   )
 
-; can also toggle global-font-lock-mode temporarily
+;; NOTE: the above didn't work, some backgrounds are still being set in some cases (comments in one case)
+;; Probably easier to toggle global-font-lock-mode temporarily as needed
+;; (terminal vim seems to do this really well, not sure why emacs struggles with it)
 
-;(unless window-system (custom-set-faces '(default ((t (:background "unspecified-bg"))))))
-
-; Preferred option
-;(unless window-system (set-face-attribute 'default nil :background "unspecified-bg"))
+;; seems like the main issue is that comments have text that gets copied to the end of the line (maybe other sections too)
