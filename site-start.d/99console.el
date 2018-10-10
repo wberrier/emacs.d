c5bc6f33577ed3cd15095b7992ac59397c26f1b9
Wade Berrier <wberrier@gmail.com>
2018-10-10 14:33:23 -0600

    console: add note about toggling global-font-lock-mode not always working

--- site-start.d/99console.el
+++ site-start.d/99console.el
@@ -5,7 +5,9 @@
 ;;   )
 
 ;; NOTE: the above didn't work, some backgrounds are still being set in some cases (comments in one case)
-;; Probably easier to toggle global-font-lock-mode temporarily as needed
 ;; (terminal vim seems to do this really well, not sure why emacs struggles with it)
 
 ;; seems like the main issue is that comments have text that gets copied to the end of the line (maybe other sections too)
+
+;; NOTE: toggling global-font-lock-mode doesn't always work either :(
+
