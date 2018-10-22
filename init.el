718d22859a1124a05fe324f3ffed4603e8b3a487
Wade Berrier <wberrier@gmail.com>
2018-10-22 09:59:23 -0600

    custom-variables: don't dirty init.el
    write out to a custom file instead

--- init.el
+++ init.el
@@ -27,16 +27,9 @@
 (autoload 'my-site-start "~/.emacs.d/manual-packages/my-site-start/my-site-start" nil t)
 (my-site-start "~/.emacs.d/site-start.d/")
 
-;; Variables set by 'customize'
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;; Write out to this file so this file isn't constantly modified
+(setq custom-file "~/.emacs.d/custom.el")
+
+;; Don't need to load it for now since most customizations are set
+;; using "use-package"
+;;(load custom-file :noerror)
