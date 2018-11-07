4d82dd875c8d537b1fba888b2442fb2142e0d665
Wade Berrier <wberrier@gmail.com>
2018-11-07 15:18:10 -0700

    javascript: add note about it not working

--- site-start.d/60js.el
+++ site-start.d/60js.el
@@ -1,5 +1,7 @@
 
 (use-package lsp-javascript-typescript
   :init
+  ;; Something wrong when editing some files, interupts typing flow...
+  ;; will have to investigate later
   (add-hook 'js-mode-hook #'lsp-javascript-typescript-enable)
   )
