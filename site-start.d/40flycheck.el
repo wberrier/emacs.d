11657b24734f673a0fae22a8c975df8c299e0268
Wade Berrier <wberrier@gmail.com>
2018-12-01 16:28:50 -0700

    python: default to python3, run both flake8 and pylint
    Should help my python best practices
    
    Some update (maybe package update?) broke eldoc with python...
    
    It's been problematic for some time... :(

--- site-start.d/40flycheck.el
+++ site-start.d/40flycheck.el
@@ -1,12 +1,19 @@
 
 (use-package flycheck
   :init
+  ;; Run pylint after running flake8
+  (flycheck-add-next-checker 'python-flake8 'python-pylint)
   ;;:config
   ;; it's a bit overkill... just enable for each mode for now
   ;;(global-flycheck-mode)
   :custom
-  (flycheck-error-list-minimum-level (quote warning))
+  ;;(flycheck-error-list-minimum-level (quote warning))
 
   (flycheck-checker-error-threshold 1000)
+
+  ;; not sure how to change these dynamically, so at least allow changing as necessary
+  (flycheck-python-flake8-executable "python3")
+  (flycheck-python-pylint-executable "python3")
+  (flycheck-python-pycompile-executable "python3")
   )
 
--- site-start.d/50python.el
+++ site-start.d/50python.el
@@ -1,5 +1,18 @@
 
+;; eldoc isn't playing nicely with my python setup... (as of Dec '18... ?)
+(defun disable-eldoc ()
+    (eldoc-mode 0)
+    )
+
 ;; NOTE: python-mode seems to detect indentation settings automatically
+(use-package python
+  :ensure nil
+  :init
+  (add-hook 'python-mode-hook #'disable-eldoc)
+  :custom
+  ;; Not sure how to change this dynamically, so at least know what to change
+  (python-shell-interpreter "python3")
+  )
 
 (use-package lsp-python
   :after (lsp-mode)
