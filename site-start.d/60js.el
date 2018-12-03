3843306f7e70709c93deca4b3595595822b2ed35
Wade Berrier <wberrier@gmail.com>
2018-12-03 11:34:33 -0700

    lsp javascript: disable for now
    interrupts typing flow in strings, not sure what's going on...

--- site-start.d/60js.el
+++ site-start.d/60js.el
@@ -3,5 +3,6 @@
   :init
   ;; Something wrong when editing some files, interupts typing flow...
   ;; will have to investigate later
-  (add-hook 'js-mode-hook #'lsp-javascript-typescript-enable)
+  ;; Something to do with company mode
+  ;;(add-hook 'js-mode-hook #'lsp-javascript-typescript-enable)
   )
