b3b7cbc422863b50760bd0dc4922cc2124ce325e
Wade Berrier <wberrier@gmail.com>
2018-12-11 16:51:21 -0700

    epa: some additional customizations

--- site-start.d/50encryption.el
+++ site-start.d/50encryption.el
@@ -1,9 +1,11 @@
 ;; This doesn't seem to work to force command line entry
 ;;(setenv "GPG_AGENT_INFO" nil)
 
-;; These seem to be enabled by default
-;;(require 'epa-file)
-;;(epa-file-enable)
+(use-package epa-file
+  :ensure nil
+  :init
+  (epa-file-enable)
+  )
 
 ;; On some versions of emacs the gpg2 pinentry was very problematic
 ;; One way to work around this is to use gpg1
@@ -21,10 +23,26 @@
 
 ;; With newer versions of emacs and gpg, you can set the pin entry
 ;; give this another try
-(setf epa-pinentry-mode 'loopback)
+;;(setf epa-pinentry-mode 'loopback)
 ;; until then, it seems to work best with no settings using gpg2
 
-;; TODO: how to fix this on ubuntu
+;; TODO: how to fix password entry on ubuntu
 ;; TODO: probably works with a gui prompt, but doesn't work well when prompting from the command line (ubuntu)
 ;; https://unix.stackexchange.com/questions/55638/can-emacs-use-gpg-agent-in-a-terminal-at-all/278875#278875
 
+(use-package epa
+  :ensure nil
+  :custom
+  (epa-pinentry-mode 'loopback)
+)
+
+;; NOTE: the above doesn't seem to use this...
+;; this probably isn't necessary...
+(use-package epg
+  :ensure nil
+  :custom
+  (epg-gpg-program "/usr/bin/gpg2")
+  )
+
+;; TODO: need to figure out how to get this working with ubuntu 16.04...
+;; (loopback really is the best...)
