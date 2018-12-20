932cb9f865e1f8dbb06e683b5bcfb42847e2f829
Wade Berrier <wberrier@gmail.com>
2018-12-20 11:20:43 -0700

    encryption: update comments
    no changes needed to get it working on ubuntu...

--- site-start.d/50encryption.el
+++ site-start.d/50encryption.el
@@ -1,5 +1,3 @@
-;; This doesn't seem to work to force command line entry
-;;(setenv "GPG_AGENT_INFO" nil)
 
 (use-package epa-file
   :ensure nil
@@ -7,42 +5,25 @@
   (epa-file-enable)
   )
 
-;; On some versions of emacs the gpg2 pinentry was very problematic
-;; One way to work around this is to use gpg1
-;; But, on some systems (el7) /usr/bin/gpg is gpg2
-;;(setq epg-gpg-program "/usr/bin/gpg")
-;;; download compile and install gpg1 from https://www.gnupg.org/download/
-;;(setq gpg1-path (format "%s/bin/gpg" local-install-dir))
-;;(when(file-executable-p gpg1-path)
-;;  (setq epg-gpg-program gpg1-path)
-;;  )
-;; The problem with this approach is that gpg1 saves the file in a format
-;; that cannot be read by newer gpg2 versions unless an option to ignore mac
-;; is enabled
-;; -> just use gpg2 formats
-
-;; With newer versions of emacs and gpg, you can set the pin entry
-;; give this another try
-;;(setf epa-pinentry-mode 'loopback)
-;; until then, it seems to work best with no settings using gpg2
-
-;; TODO: how to fix password entry on ubuntu
-;; TODO: probably works with a gui prompt, but doesn't work well when prompting from the command line (ubuntu)
-;; https://unix.stackexchange.com/questions/55638/can-emacs-use-gpg-agent-in-a-terminal-at-all/278875#278875
-
+;; Of all the ways to ask for a password, this seems to work the best
+;; using pinentry-curses on emacs was very problematic in missing key strokes
+;; I tried setting environment variables to force command line entry,
+;; but that didn't work either
 (use-package epa
   :ensure nil
   :custom
   (epa-pinentry-mode 'loopback)
 )
 
-;; NOTE: the above doesn't seem to use this...
-;; this probably isn't necessary...
+;; Be sure to use gpg2 and related formats
+;; The problem with gpg1 is that it saves the file in a format that
+;; cannot be read by newer gpg2 versions unless an option to ignore
+;; mac is enabled
+;; -> just use gpg2 formats
+;; NOTE: epa doesn't seem to use this setting
+;; and is probably not necessary...
 (use-package epg
   :ensure nil
   :custom
   (epg-gpg-program "/usr/bin/gpg2")
   )
-
-;; TODO: need to figure out how to get this working with ubuntu 16.04...
-;; (loopback really is the best...)
