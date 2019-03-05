de9ee95124fc241c4908d796e91daab8ed31f234
Wade Berrier <wberrier@gmail.com>
2019-03-05 11:22:29 -0700

    shell: colorize compile output
    yay!  Was looking into disabling cmake color output, but just decided
    to enable color output, and this works great

--- site-start.d/50shell.el
+++ site-start.d/50shell.el
@@ -4,3 +4,23 @@
   :init (add-hook 'sh-mode-hook #'lsp)
   :mode ("bash_profile" "bashrc")
   )
+
+;; make compilation output color nice
+(use-package xterm-color
+  :init
+  ;; got this hook from the xterm-color docs
+  (add-hook 'compilation-start-hook
+            (lambda (proc)
+              ;; We need to differentiate between compilation-mode buffers
+              ;; and running as part of comint (which at this point we assume
+              ;; has been configured separately for xterm-color)
+              (when (eq (process-filter proc) 'compilation-filter)
+                ;; This is a process associated with a compilation-mode buffer.
+                ;; We may call `xterm-color-filter' before its own filter function.
+                (set-process-filter
+                 proc
+                 (lambda (proc string)
+                   (funcall 'compilation-filter proc
+                            (xterm-color-filter string)))))))
+
+  )
