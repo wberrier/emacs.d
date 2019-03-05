026ed1b83e4e958f2ed6b9a7bf9b3d97f9da693c
Wade Berrier <wberrier@gmail.com>
2019-03-05 13:31:24 -0700

    emacs settings: add file and enable visiting file linked to
    this helps when jumping to errors in compilation mode
    
    Needed because the compilation output is from a docker container which
    has different paths

---
+++ site-start.d/02emacs.el
@@ -0,0 +1,7 @@
+
+;; Various emacs settings
+
+;; Visit the actual file when it's a symlink
+;; Really this is for the compilation buffer to follow files
+;; TODO: any drawbacks for doing this?
+(setq find-file-visit-truename t)
