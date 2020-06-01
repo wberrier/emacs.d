1f53a86a5b5f767ad4b05074132716c2ddfeafdc
Wade Berrier <wberrier@gmail.com>
2020-06-01 14:43:36 -0600

    add this ansible config that's been laying around

---
+++ site-start.d/60ansible.el
@@ -0,0 +1,31 @@
+
+(use-package ansible
+  :init
+  (add-hook 'yaml-mode-hook '(lambda () (ansible 1)))
+  (add-hook 'ansible-hook 'ansible-auto-decrypt-encrypt)
+  (setq ansible-vault-password-file "~/.vault-pass")
+  )
+
+
+(use-package ansible-doc
+  :init
+  (add-hook 'yaml-mode-hook #'ansible-doc-mode)
+  )
+
+(use-package company-ansible
+  :config
+  (add-to-list 'company-backends 'company-ansible)
+  )
+
+(defun ansible-vault-mode-maybe ()
+  (when (ansible-vault--is-vault-file)
+	(ansible-vault-mode 1)))
+
+(use-package ansible-vault
+  :init
+  (add-hook 'yaml-mode-hook 'ansible-vault-mode-maybe)
+  )
+
+;; not sure how to configure this one? or if it's necessary?
+;; (use-package poly-ansible
+;;   )
