3957850eaaa69a979a0538d52181f0ee0c34455d
Wade Berrier <wberrier@gmail.com>
2020-06-01 14:45:19 -0600

    add restart-emacs

--- site-start.d/02emacs.el
+++ site-start.d/02emacs.el
@@ -5,3 +5,8 @@
 ;; Really this is for the compilation buffer to follow files
 ;; TODO: any drawbacks for doing this?
 (setq find-file-visit-truename t)
+
+;; add function to be able to restart emacs
+;; this should be alleviate the flow of change config, kill_emacs, start again...
+;; although, not sure it works for my use case?
+(use-package restart-emacs)
