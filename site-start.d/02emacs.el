fe28ba0b15218eb82c64b9d50d0cd2cc946264f1
Wade Berrier <wberrier@gmail.com>
2020-06-02 21:15:32 -0600

    Revert "add restart-emacs"
    This reverts commit 3957850eaaa69a979a0538d52181f0ee0c34455d.
    
    I use emacs in the terminal, and this seems to only work when running
    in gui mode anyways.

--- site-start.d/02emacs.el
+++ site-start.d/02emacs.el
@@ -5,8 +5,3 @@
 ;; Really this is for the compilation buffer to follow files
 ;; TODO: any drawbacks for doing this?
 (setq find-file-visit-truename t)
-
-;; add function to be able to restart emacs
-;; this should be alleviate the flow of change config, kill_emacs, start again...
-;; although, not sure it works for my use case?
-(use-package restart-emacs)
