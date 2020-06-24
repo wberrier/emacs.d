51fca733868a07b280aa155bb36bf3a15688c3f5
Wade Berrier <wberrier@gmail.com>
2020-06-24 15:30:46 -0600

    gui: disable menubar, toolbar, and scroll bar
    Getting closer to at some point running emacs in gui mode

--- site-start.d/05ui.el
+++ site-start.d/05ui.el
@@ -1,13 +1,15 @@
 
-;; hide toolbar when in gui mode
+;; hide menu bar (File, Edit, etc...) when in gui mode
 (menu-bar-mode -1)
 ;(text-scale-decrease) ; go from 11 to 10 (Hope that works)
 
-; these aren't working on el7 in text mode...
-; what's the correct way to get rid of the scrollbar?
-; emacs version?  only gui?
-;(tool-bar-mode -1)
-;(toggle-scroll-bar -1)
+;; Don't show the toolbar in gui mode
+;; NOTE: for cases where the toolbar is useful (gdb, dap-mode, etc...) this can always be toggled as necessary
+(tool-bar-mode -1)
+
+;; Don't show scrollbars in gui mode
+;; They look really bad, particularly when using split screen modes
+(scroll-bar-mode -1)
 
 ; show column in addition to line number in mode line
 ; interesting that this isn't the default?
