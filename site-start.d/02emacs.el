f9fc3737c18c592d81f6f90f9c1dcb3a8b615b1d
Wade Berrier <wberrier@gmail.com>
2022-08-04 23:16:27 -0600

    emacs 28.1: work around bug
    was getting a bunch of odd bugs with this emacs update...
    
    Found a solution online to work around these

--- site-start.d/02emacs.el
+++ site-start.d/02emacs.el
@@ -4,4 +4,13 @@
 ;; Visit the actual file when it's a symlink
 ;; Really this is for the compilation buffer to follow files
 ;; TODO: any drawbacks for doing this?
-(setq find-file-visit-truename t)
+;; this is currently breaking emacs 28.1
+;; https://debbugs.gnu.org/cgi/bugreport.cgi?bug=52292
+(setq find-file-visit-truename nil)
+
+;; avoid some compilation warnings, that force showing the warnings buffer...
+(use-package warnings
+  :ensure nil
+  :custom
+  (warning-suppress-types '((comp)))
+)
