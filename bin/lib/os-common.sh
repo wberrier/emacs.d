83d1d1abf09fde14c76939ef09899eadb2334aa2
Wade Berrier <wberrier@gmail.com>
2023-11-24 12:55:21 -0700

    setup-lsp-backends: use common shell libs
    to avoid duplicate functions scattered across repos

---
+++ bin/lib/os-common.sh
@@ -0,0 +1,134 @@
+# shellcheck shell=bash
+# Intended to be included to provide common functionality
+
+#
+# Returns a string containing the current Linux distribution
+#
+get_dist()
+{
+	dist="nodist"
+	if [ -e /etc/fedora-release ]; then
+		dist=$(cat /etc/fedora-release | sed -e "s/.*release\s\([0-9]\+\).*/fc\1/")
+	elif [ -e /etc/redhat-release ]; then
+		dist=$(cat /etc/redhat-release | sed -e "s/.*release\s\([0-9]\+\).*/el\1/")
+	elif [ -e /etc/debian_version ]; then
+		# TODO: does this work on olders versions than debian 10?
+		dist=$(cat /etc/debian_version | sed -e "s/\([0-9]\+\).*/Debian-\1/")
+	elif [ -e /etc/lsb-release ]; then
+		id=$(grep DISTRIB_ID /etc/lsb-release | awk -F'=' '{print $2}')
+		release=$(grep DISTRIB_RELEASE /etc/lsb-release | awk -F'=' '{print $2}')
+		dist="${id}-${release}"
+	fi
+
+	arch=$(uname -m)
+
+	# TODO: combine i[356]86?
+
+	echo "$dist-$arch"
+}
+
+package_system() {
+	case $(get_dist) in
+		fc*|el*)
+			echo "rpm"
+			;;
+		Debian*|Ubuntu*)
+			echo "dpkg"
+			;;
+		*)
+			echo "UNSUPPORTED"
+			;;
+	esac
+}
+
+install_packages() {
+	case $(package_system) in
+		rpm)
+			# el7 uses yum still... don't refer to dnf...
+			sudo yum install -y --best --allowerasing "$@"
+			;;
+		dpkg)
+			sudo apt-get update
+			sudo apt-get install -y "$@"
+			;;
+		*)
+			echo "unsupported platform"
+			;;
+	esac
+}
+
+remove_packages() {
+	case $(package_system) in
+		rpm)
+			sudo yum remove -y "$@"
+			;;
+		dpkg)
+			sudo apt-get remove --purge -y "$@"
+			;;
+		*)
+			echo "unsupported platform"
+			;;
+	esac
+}
+
+list_package_contents() {
+	case $(package_system) in
+		rpm)
+			rpm -ql "$@"
+			;;
+		dpkg)
+			dpkg -L "$@"
+			;;
+		*)
+			echo "unsupported platform"
+			;;
+	esac
+}
+
+package_installed()
+{
+	case $(package_system) in
+		rpm)
+			rpm -q "$1" > /dev/null
+			;;
+		dpkg)
+			dpkg -s "$1" 2>/dev/null | grep Status: | grep -q installed
+			;;
+		*)
+			echo "unsupported platform"
+			false
+			;;
+	esac
+}
+
+install_rpm_from_url_if_not_installed()
+{
+	url="$1"
+	rpm_name="$2"
+
+	if ! package_installed "$rpm_name" ; then
+		install_packages "$url"
+	else
+		echo "$2 already installed"
+	fi
+}
+
+install_python_packages()
+{
+	pip_args=""
+	case $(get_dist) in
+		# Looks like debian patches pip to required this option?
+		Debian*|Ubuntu*)
+			pip_args="--break-system-packages"
+			;;
+	esac
+
+	pip3 install --user $pip_args "$@"
+}
+
+install_flatpaks()
+{
+	# NOTE: running on debian seemed to require sudo
+	# Not sure if there's an alternative?
+	sudo flatpak install -y --noninteractive --system "$@"
+}
---
+++ bin/lib/vcs-common.sh
@@ -0,0 +1,100 @@
+
+repo_dir()
+{
+	repo="$1"
+
+	repo_dir=$(basename "$repo")
+	# Remove .git extension
+	repo_dir=${repo_dir/.git/}
+
+	echo "$repo_dir"
+}
+
+latest_git_tag()
+{
+	git tag -l | sort -V | tail -n1
+}
+
+clone_or_update_git_repo_latest_tag()
+{
+	url="$1"
+
+	# Must clone first, to make sure it exists
+	clone_or_update_git_repo "$url"
+
+	repo_dir=$(repo_dir "$url")
+	pushd $repo_dir
+	latest_tag=$(latest_git_tag)
+	popd
+
+	clone_or_update_git_repo "$url" "$latest_tag"
+}
+
+update_git_submodules() {
+	optional_submodule="$1"
+	git submodule sync
+	git submodule update --init --recursive "$optional_submodule"
+}
+
+# This assums that the repo doesn't contain the .repo extension
+clone_or_update_git_repo()
+{
+	repo=$1
+
+	if [ ! -z "$2" ] ; then
+		revision="$2"
+	else
+		revision="master"
+	fi
+
+	if [ ! -z "$3" ] ; then
+		repo_dir=$3
+	else
+		repo_dir=$(repo_dir "$repo")
+	fi
+
+	echo "Cloning or updating repo: $repo"
+
+	if [ ! -e $repo_dir ] ; then
+		git clone $repo $repo_dir
+		pushd $repo_dir
+		git config user.name "Wade Berrier"
+		git config user.email "wberrier@gmail.com"
+		popd
+	else
+		pushd $repo_dir
+		# Do a fetch before doing a pull
+		git fetch -pa
+		git pull
+		popd
+	fi
+
+	pushd $repo_dir
+	git checkout $revision
+	# handle any submodules
+	update_git_submodules
+	popd
+}
+
+clone_or_update_mercurial_repo()
+{
+	repo=$1
+
+	repo_dir=$(basename $repo)
+
+	echo "Cloning or updating repo: $repo"
+
+	if [ ! -e $repo_dir ] ; then
+		hg clone $repo
+
+		# Set username for these repos (usually public)
+		echo '[ui]' >> $repo_dir/.hg/hgrc
+		echo 'username = Wade Berrier <wberrier@gmail.com>' >> $repo_dir/.hg/hgrc
+
+	else
+		pushd $repo_dir
+		hg pull
+		hg update
+		popd
+	fi
+}
