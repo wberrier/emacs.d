a8d233a064a631daacc0fb250116c5a7c8ebff47
Wade Berrier <wberrier@gmail.com>
2024-02-16 21:38:07 -0700

    encryption: looks like pinentry got moved from epa to epg
    also don't specify the gpg program any longer.  Looks like emacs
    figures out the right one to use, and debian doesn't have "gpg2".

--- site-start.d/50encryption.el
+++ site-start.d/50encryption.el
@@ -5,18 +5,6 @@
   (epa-file-enable)
   )
 
-;; Of all the ways to ask for a password, this seems to work the best
-;; using pinentry-curses on emacs was very problematic in missing key strokes
-;; I tried setting environment variables to force command line entry,
-;; but that didn't work either
-(use-package epa
-  :ensure nil
-  :custom
-  ;; NOTE: this probably requires emacs 25.1
-  ;; seems to default to older pinentry otherwise
-  (epa-pinentry-mode 'loopback)
-)
-
 ;; Be sure to use gpg2 and related formats
 ;; The problem with gpg1 is that it saves the file in a format that
 ;; cannot be read by newer gpg2 versions unless an option to ignore
@@ -27,5 +15,15 @@
 (use-package epg
   :ensure nil
   :custom
-  (epg-gpg-program "/usr/bin/gpg2")
+  ;; newer versions of emacs have min versions of gpg1 and gpg2
+  ;; the modern versions of distros I use all have gpg v2
+  ;;(epg-gpg-program "/usr/bin/gpg2")
+
+  ;; Of all the ways to ask for a password, this seems to work the best
+  ;; using pinentry-curses on emacs was very problematic in missing key strokes
+  ;; I tried setting environment variables to force command line entry,
+  ;; but that didn't work either
+  ;; NOTE: this probably requires emacs 25.1
+  ;; seems to default to older pinentry otherwise
+  (epg-pinentry-mode 'loopback)
   )
