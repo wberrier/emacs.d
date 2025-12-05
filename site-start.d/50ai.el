026d0afe56d83967df1028db8493d8ba48a0e11d
Wade Berrier <wberrier@gmail.com>
2025-12-05 15:28:00 -0700

    add agent-shell, yay!

---
+++ site-start.d/50ai.el
@@ -0,0 +1,8 @@
+;; agent-shell
+(use-package agent-shell
+  :config
+  ;; Configure to run opencode commands
+  ;;(setq agent-shell-default-command "opencode")
+  ;; (setq agent-shell-google-authentication
+  ;;       (agent-shell-google-make-authentication :login t))
+  )
